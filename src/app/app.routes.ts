13ca0b89c9103ae97848ab08e1a4fbff6fc4a2c7
Sascha Vis <saschavis@gmail.com>
2024-09-15 09:34:14 +0200

    feat: First iteration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
-import { Routes } from '@angular/router';
+import { provideRouter, Routes, withDebugTracing } from '@angular/router';
+import { HintComponent } from './pages/hint/hint.component';
+import { ApplicationConfig } from '@angular/core';
+import { GeneratorPageComponent } from './pages/generator-page/generator-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'hint/:hintparams', component: HintComponent },
+  { path: 'admin/generator', component: GeneratorPageComponent },
+  { path: '', redirectTo: '/admin/generator', pathMatch: 'full' },
+  //   { path: '', redirectTo: '/hint', pathMatch: 'full' }, // Default route
+];
